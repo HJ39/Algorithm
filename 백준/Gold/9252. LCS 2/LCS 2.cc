44a12b0194fc681152cf877ab371a6c2a3ffb08e
HJ <81843677+HJ39@users.noreply.github.com>
2023-10-21 20:57:05 +0900

    [Gold IV] Title: LCS 2, Time: 4 ms, Memory: 5816 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define fast ios::sync_with_stdio(false),cin.tie(NULL), cout.tie(NULL)
+
+using namespace std;
+
+int main(){
+    fast;
+    
+    string a,b; cin>>a>>b;
+    
+    int lcs[1001][1001];
+    int aLength = a.size();
+    int bLength = b.size();
+    
+    for(int i=0; i<=aLength; ++i){
+        for(int j=0; j<=bLength; ++j){
+            if(i == 0 or j == 0)
+                lcs[i][j] = 0;
+            else if(a[i-1] == b[j-1])
+                lcs[i][j] = lcs[i-1][j-1] + 1;
+            else
+                lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1]);
+        }
+    }
+    
+    cout<<lcs[aLength][bLength]<<"\n";
+    stack<char> st;
+    
+    while (aLength>0 and bLength>0) {
+        if(lcs[aLength][bLength-1] == lcs[aLength][bLength])
+            bLength--;
+        else if(lcs[aLength-1][bLength] == lcs[aLength][bLength])
+            aLength--;
+        else{
+            aLength--;
+            bLength--;
+            st.push(a[aLength]);
+        }
+    }
+    
+    while (!st.empty()) {
+        cout<<st.top();
+        st.pop();
+    }
+    cout<<"\n";
+    
+}

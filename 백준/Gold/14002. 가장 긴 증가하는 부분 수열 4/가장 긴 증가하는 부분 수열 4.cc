c5e254f59bced5e20b572f9672d4933a5911382a
HJ <81843677+HJ39@users.noreply.github.com>
2023-10-24 00:13:17 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    vector<int> array;
+    for(int i=0;i<n;i++){
+        int num;
+        scanf("%d",&num);
+        array.push_back(num);
+    }
+
+    vector<int> lis;
+    vector<int> backTrace;
+    lis.push_back(array[0]);
+
+    for(int i=0;i<n;i++){
+        if(lis.back() < array[i]){
+            lis.push_back(array[i]);
+            backTrace.push_back(lis.size() - 1);
+        }
+        else{
+            long index = distance(lis.begin(), lower_bound(lis.begin(), lis.end(), array[i]));
+            backTrace.push_back(index);
+            lis[index] = array[i];
+        }
+    }
+
+    printf("%d\n",lis.size());
+    
+    int temp = lis.size()-1;
+    vector<int> result;
+    for(int i= backTrace.size()-1; i>=0;i--){
+        if(backTrace[i] == temp){
+            temp--;
+            result.push_back(array[i]);
+        }
+    }
+ 
+    sort(result.begin(),result.end());
+    
+    for(int i=0;i<result.size();i++)
+        printf("%d ",result[i]);
+    printf("\n");
+    
+}

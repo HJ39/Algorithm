0c4cfce4736fb28ab7b8c7e1ee8303aced22ee35
HJ <81843677+HJ39@users.noreply.github.com>
2023-12-23 16:17:58 +0900

    [Gold V] Title: 달력, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

---
+++ 백준/Gold/20207. 달력/달력.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+
+using namespace std;
+
+vector<pair<int,int>> input;
+int check[1001];
+
+int main(){
+    fast;
+    
+    int N; cin>>N;
+    
+    for(int i=0; i<N; ++i) {
+        int s,e; cin>>s>>e;
+        input.push_back({s,e});
+    }
+    
+    sort(input.begin(), input.end());
+    
+    int res = 0;
+    int finalLast = 0;
+    
+    for(int i=0; i<N; ++i) {
+        auto [s,e] = input[i];
+        
+        for(int j=s; j<=e; ++j) {
+            check[j]++;
+        }
+        
+        finalLast = max(finalLast, e);
+    }
+    
+    int s = 1;
+    int height = 0;
+    for(int i=1; i<=finalLast+1; ++i) {
+        if(check[i] != 0) {
+            height = max(height, check[i]);
+        } else {
+            res += (i-s) * height;
+            s = i+1;
+            height = 0;
+        }
+    }
+    
+    cout<<res<<"\n";
+}

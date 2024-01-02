0037fae4136033f55c76c69ebc11028661457456
HJ <81843677+HJ39@users.noreply.github.com>
2024-01-02 09:37:49 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.cc
+++ 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.cc
@@ -3,24 +3,27 @@
 #define MAX 987654321
 using namespace std;
 
-int input[7][7], dp[7][7][4];
+int input[7][7], check[7];
 int N, M;
 int dd[] = {-1, 0, 1, MAX};
+int res = MAX;
 
-// sol
-// 0: 왼, 1: 가, 2: 오
-int sol(int x, int y, int past) {
-    if(1>y or y>M) return MAX;
-    if(x == N)  return input[x][y];
-    int &val = dp[x][y][past];
-    if(val != MAX) return val;
-    
-    for(int i=0; i<3; ++i) {
-        if(i == past) continue;
-        val = min(val, input[x][y] + sol(x+1, y + dd[i] , i));
+void dfs(int x, int y, int past) {
+    if(x == N+1) {
+        int sum = 0;
+        for(int i=1; i<=N; ++i) {
+            sum += input[i][check[i]];
+        }
+        res = min(res, sum);
+        return;
     }
-    
-    return val;
+  
+    for(int i=0; i<3; ++i) {
+        int dy = y + dd[i];
+        if(dy<1 or dy>M or past == i) continue;
+        check[x] = dy;
+        dfs(x+1, dy, i);
+    }
 }
 
 int main(){
@@ -30,12 +33,9 @@ int main(){
     for(int i=1; i<=N; ++i)
         for(int j=1; j<=M; ++j)
             cin>>input[i][j];
-    
-    fill(&dp[0][0][0], &dp[6][6][4], MAX);
-  
-    int res = MAX;
-    for(int j=1; j<=M; ++j)
-        res = min(res, sol(1, j, 3));
 
+    for(int j=1; j<=M; ++j)
+        dfs(1, j, 3);
+    
     cout<<res<<"\n";
 }

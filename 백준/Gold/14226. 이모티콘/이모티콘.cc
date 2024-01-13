6484adea73d52ada8d38a12099c5a5fb1f53d4e3
HJ <81843677+HJ39@users.noreply.github.com>
2024-01-13 17:11:17 +0900

    [Gold IV] Title: 이모티콘, Time: 0 ms, Memory: 3132 KB -BaekjoonHub

---
+++ 백준/Gold/14226. 이모티콘/이모티콘.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+
+using namespace std;
+
+bool check[1001][1001];
+int S;
+
+int bfs() {
+    queue<tuple<int, int, int>> q;
+    
+    q.push({1, 0, 0});
+    
+    while (!q.empty()) {
+        auto [screen, clip, time] = q.front();
+        q.pop();
+        
+        if(screen == S) return time;
+        
+        if(screen > 1000 or screen <= 0) continue;
+        
+        if(!check[screen][screen]) {
+            check[screen][screen] = true;
+            q.push({screen, screen, time+1});
+        }
+        
+        if(!check[screen-1][screen]) {
+            check[screen-1][screen] = true;
+            q.push({screen-1, clip, time+1});
+        }
+        
+        if(screen + clip <= 1000) {
+            if(!check[screen + clip][screen]) {
+                check[screen + clip][screen] = true;
+                q.push({screen + clip, clip, time+1});
+            }
+        }
+    }
+    return 0;
+}
+
+int main() {
+    fast;
+    cin>>S;
+    
+    cout<<bfs()<<"\n";
+}
